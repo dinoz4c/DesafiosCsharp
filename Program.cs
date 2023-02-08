38b951f169a8c7320c8739ac519253aa8b41d5b1
dinoz4c <124808570+dinoz4c@users.noreply.github.com>
2023-02-08 14:11:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafiosLuizao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            desafios Lista = new desafios();
+            Lista.Desafio030();
+        }
+    }
+}
